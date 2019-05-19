2ff29e9080763e526cbd5da79cd272fe48cdbd96
Adrian Araujo <adrianaraujo247@gmail.com>
2019-05-19 01:57:39 -0300

    criando os primeiros componentes

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+        <h1>Problema de Monty Hall</h1>
+        <Gift />
+    </div>
+</template>
+
+<script>
+import Gift from '.component/Gift'
+export default {
+    name:'App',
+    components:{ Gift }
+}
+</script>
+
+<style>
+    *{
+        box-sizing : border-box;
+        font-family: 'Montserrat', sans-serif;
+        background: linear-gradient(to right, #c33764, #1d2671);
+    }
+    #app {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    #app h1 {
+        border: 1px solid black;
+        background-color:#0004;
+        padding: 20px;
+        margin-bottom: 60px;
+    }
+</style>
